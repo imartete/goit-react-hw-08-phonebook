a73cc7fc6cd7fdafc51badb6e748aa57825de9f3
Marta <maardiakiv@gmail.com>
2023-01-11 16:29:53 +0100

    update App with Redux, useSelectors

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,29 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { Section } from 'components/Section/Section';
 import { Notification } from 'components/Notification/Notification';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/SearchFilter/SearcFilter';
-import { nanoid } from 'nanoid';
 
-const LOCAL_STORAGE_KEY = 'contacts';
+// const LOCAL_STORAGE_KEY = 'contacts';
 
 export function App() {
-  const [contacts, setContact] = useState(
-    () => JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
-  );
-  const [filter, setFilter] = useState('');
+  const contacts = useSelector(state => state.contacts.contacts);
+  const filter = useSelector(state => state.filter.value);
 
-  useEffect(
-    () => localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts)),
-    [contacts]
-  );
+  // const [contacts, setContact] = useState(
+  //   () => JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  // );
 
-  function addContact({ name, number }) {
-    const existingContact = contacts.some(
-      contact => contact.name.toLowerCase() === name.toLowerCase()
-    );
-
-    if (!existingContact) {
-      setContact(prevContacts => [
-        { name, number, id: nanoid() },
-        ...prevContacts,
-      ]);
-      return;
-    }
-    alert(`${name} is alredy in contacts.`);
-  }
-
-  function removeContact(contactId) {
-    setContact(contacts.filter(el => el.id !== contactId));
-  }
-
-  function handleSearch(value) {
-    setFilter(value);
-  }
+  // useEffect(
+  //   () => localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts)),
+  //   [contacts]
+  // );
 
   function getVisibleContacts() {
     const normalizedFilter = filter.toLowerCase().trim();
-
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
@@ -54,16 +32,13 @@ export function App() {
   return (
     <div>
       <Section title={'Phonebook'}>
-        <ContactForm onSubmit={addContact} />
+        <ContactForm />
       </Section>
       <Section title={'Contacts'}>
         {contacts.length ? (
           <>
-            <Filter searchItemHandler={handleSearch} value={filter} />
-            <ContactList
-              contacts={getVisibleContacts()}
-              removeItem={removeContact}
-            />
+            <Filter />
+            <ContactList contacts={getVisibleContacts()} />
           </>
         ) : (
           <Notification message="No contacts added" />
