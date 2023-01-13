4078a988a6c0b90cb36422ff0cb1354d87169797
Marta <maardiakiv@gmail.com>
2023-01-14 00:06:06 +0100

    add Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,22 @@
+import './Loader.modules.css';
+
+export function Loader() {
+  return (
+    <div className="overlay">
+      <div className="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
