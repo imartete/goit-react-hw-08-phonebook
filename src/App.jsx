9ad8bc0cf6121eddecfc5bb006cfcec7104cf890
imartete <79273837+imartete@users.noreply.github.com>
2023-01-17 15:08:42 +0100

    add core routers to App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,50 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchContacts } from 'redux/contacts/operations';
-import {
-  selectContacts,
-  selectError,
-  selectLoading,
-} from 'redux/contacts/selectors';
-import { selectVisibleContacts } from 'redux/selectorsGlobal';
-import { Section } from 'components/Section/Section';
-import { Notification } from 'components/Notification/Notification';
-import { ContactForm } from 'components/ContactForm/ContactForm';
-import { ContactList } from 'components/ContactList/ContactList';
-import { Filter } from 'components/SearchFilter/SearcFilter';
-import { Loader } from 'components/Loader/Loader';
+import { lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { RestrictedRoute } from 'components/RestrictedRoute';
+import { Layout } from 'components/Layout';
+import { PrivateRoute } from 'components/PrivateRoute';
+
+const HomePage = lazy(() => import('./pages/Home'));
+const RegisterPage = lazy(() => import('./pages/Register'));
+const LoginPage = lazy(() => import('./pages/Login'));
+const ContactsPage = lazy(() => import('./pages/Contacts'));
 
 export function App() {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const isLoading = useSelector(selectLoading);
-  const error = useSelector(selectError);
-  const visibleContacts = useSelector(selectVisibleContacts);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
 
   return (
     <>
-      <Section title={'Phonebook'}>
-        <ContactForm />
-      </Section>
-      <Section title={'Contacts'}>
-        {contacts && !error && !isLoading && (
-          <>
-            <Filter />
-            <ContactList contacts={visibleContacts} />
-          </>
-        )}
-        {!contacts.length && !error && !isLoading && (
-          <Notification message="No contacts added" />
-        )}
-        {isLoading && <Loader />}
-        {error && <p>An error has occured. {error}</p>}
-      </Section>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<RegisterPage />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<LoginPage />}
+              />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          />
+        </Route>
+      </Routes>
     </>
   );
 }
