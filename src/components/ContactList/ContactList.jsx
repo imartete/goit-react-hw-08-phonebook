0cf4fbfbec2689370549a6199a6f026a2b3867b5
imartete <79273837+imartete@users.noreply.github.com>
2023-01-17 15:04:22 +0100

    pass an array via useSelector, not as a prop

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,21 @@
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import { ContactListItem } from '../ContactListItem/ContactListItem';
+import { selectVisibleContacts } from 'redux/selectorsGlobal';
 import './ContactList.modules.css';
 
-export const ContactList = ({ contacts }) => {
+export function ContactList() {
+  const visibleContacts = useSelector(selectVisibleContacts);
+
   return (
     <ul>
-      {contacts.map(contact => (
+      {visibleContacts.map(contact => (
         <ContactListItem
           key={contact.id}
           id={contact.id}
           name={contact.name}
-          phone={contact.phone}
+          number={contact.number}
         />
       ))}
     </ul>
   );
-};
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      phone: PropTypes.string.isRequired,
-    }).isRequired
-  ).isRequired,
-};
+}
