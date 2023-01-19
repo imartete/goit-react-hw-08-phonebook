42e1232a593b5ff221f5a4ee922707412dea9bc3
imartete <79273837+imartete@users.noreply.github.com>
2023-01-19 21:47:16 +0100

    update contact form and validation with mantine

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,69 +1,73 @@
 import React from 'react';
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectContacts } from 'redux/contacts/selectors';
 import './ContactForm.modules.css';
 import { addContact } from 'redux/contacts/operations';
 import { Button, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { showNotification } from '@mantine/notifications';
 
 export function ContactForm() {
   const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
 
-  const existingContact = contacts.some(
-    contact => contact.name.toLowerCase() === name.toLowerCase()
-  );
+  const form = useForm({
+    initialValues: { name: '', number: '' },
 
-  function handleChange(event) {
-    const { name, value } = event.target;
-    if (name === 'name') setName(value);
-    if (name === 'number') setNumber(value);
-  }
+    validate: {
+      name: value => {
+        const existingContact = contacts.some(
+          contact => contact.name.toLowerCase() === value.toLowerCase()
+        );
+        if (existingContact)
+          showNotification({
+            color: 'red',
+            message: `${value} is already in contacts`,
+          });
+        return value.length &&
+          /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/.test(
+            value
+          ) &&
+          !existingContact
+          ? null
+          : 'Invalid name';
+      },
+      number: value =>
+        /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/.test(
+          value
+        )
+          ? null
+          : 'Invalid number',
+    },
+  });
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (existingContact) {
-      alert('Already in contacts');
-      return;
-    }
-    setName('');
-    setNumber('');
-    dispatch(addContact({ name, number }));
+  function handleSubmit(value) {
+    dispatch(addContact(value));
+    form.reset();
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name
-        <TextInput
-          type="text"
-          name="name"
-          value={name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Number
-        <TextInput
-          type="tel"
-          name="number"
-          value={number}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          onChange={handleChange}
-        />
-      </label>
+    <form onSubmit={form.onSubmit(handleSubmit)}>
+      <TextInput
+        sx={{ maxWidth: 300 }}
+        label="Username"
+        type="text"
+        withAsterisk
+        {...form.getInputProps('name')}
+      />
+      <TextInput
+        sx={{ maxWidth: 300 }}
+        label="Number"
+        type="phone"
+        withAsterisk
+        {...form.getInputProps('number')}
+      />
       <Button
         type="submit"
         variant="gradient"
         gradient={{ from: 'indigo', to: '#BAC8FF' }}
         sx={{
+          width: 200,
           fontSize: '16px',
         }}
       >
