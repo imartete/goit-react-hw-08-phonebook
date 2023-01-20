652ba5d074aac53d7d1c85bd77980656359d3199
imartete <79273837+imartete@users.noreply.github.com>
2023-01-20 12:08:51 +0100

    display error, ensure error reset on tab change

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,19 @@
-import { Button, PasswordInput, TextInput } from '@mantine/core';
+import { Alert, Button, PasswordInput, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
-import { IconAt, IconKey } from '@tabler/icons';
+import { IconAlertCircle, IconAt, IconKey } from '@tabler/icons';
+import { useAuth } from 'hooks/useAuth';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { reduceError } from 'redux/user/userSlice';
 import { logIn } from '../redux/user/operations';
 
 export function LoginForm() {
   const dispatch = useDispatch();
+  const { error } = useAuth();
+
+  useEffect(() => {
+    dispatch(reduceError());
+  }, [dispatch]);
 
   const form = useForm({
     initialValues: { email: '', password: '' },
@@ -16,40 +24,43 @@ export function LoginForm() {
     },
   });
 
-  // TODO: error handling with redux
-
   function handleSubmit(values) {
-    console.log(values);
     dispatch(logIn(values));
-    form.reset();
   }
 
   return (
-    <form onSubmit={form.onSubmit(handleSubmit)} autoComplete="off">
-      <TextInput
-        sx={{ maxWidth: 300 }}
-        label="Email"
-        // type="email"
-        name="email"
-        withAsterisk
-        icon={<IconAt size={14} />}
-        {...form.getInputProps('email')}
-      />
-      <PasswordInput
-        sx={{ maxWidth: 300 }}
-        label="Password"
-        name="password"
-        withAsterisk
-        icon={<IconKey size={14} />}
-        {...form.getInputProps('password')}
-      />
-      <Button
-        type="submit"
-        variant="gradient"
-        gradient={{ from: 'teal', to: 'lime', deg: 105 }}
-      >
-        Log In
-      </Button>
-    </form>
+    <>
+      <form onSubmit={form.onSubmit(handleSubmit)} autoComplete="off">
+        <TextInput
+          sx={{ maxWidth: 300 }}
+          label="Email"
+          // type="email"
+          name="email"
+          withAsterisk
+          icon={<IconAt size={14} />}
+          {...form.getInputProps('email')}
+        />
+        <PasswordInput
+          sx={{ maxWidth: 300 }}
+          label="Password"
+          name="password"
+          withAsterisk
+          icon={<IconKey size={14} />}
+          {...form.getInputProps('password')}
+        />
+        <Button
+          type="submit"
+          variant="gradient"
+          gradient={{ from: 'teal', to: 'lime', deg: 105 }}
+        >
+          Log In
+        </Button>
+      </form>
+      {error && (
+        <Alert icon={<IconAlertCircle size={16} />} title="Bummer!" color="red">
+          User not found. Invalid data entered.
+        </Alert>
+      )}
+    </>
   );
 }
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -4,10 +4,16 @@ import { useDispatch } from 'react-redux';
 import { register } from '../redux/user/operations';
 import { useForm } from '@mantine/form';
 import { useAuth } from 'hooks/useAuth';
+import { useEffect } from 'react';
+import { reduceError } from 'redux/user/userSlice';
 
 export function RegisterForm() {
   const dispatch = useDispatch();
   const { error } = useAuth();
+
+  useEffect(() => {
+    dispatch(reduceError());
+  }, [dispatch]);
 
   const form = useForm({
     initialValues: { name: '', email: '', password: '' },
@@ -17,53 +23,53 @@ export function RegisterForm() {
         value.length < 2 ? 'Name must have at least 2 characters' : null,
       email: value => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
       password: value =>
-        value.length < 10 ? 'Password must have at least 10 characters' : null,
+        value.length < 7 ? 'Password must have at least 7 characters' : null,
     },
   });
 
   function handleSubmit(values) {
-    console.log(values);
     dispatch(register(values));
-    form.reset();
   }
 
   return (
-    <form onSubmit={form.onSubmit(handleSubmit)} autoComplete="off">
-      <TextInput
-        sx={{ maxWidth: 300 }}
-        label="Username"
-        type="text"
-        withAsterisk
-        icon={<IconUser size={14} />}
-        {...form.getInputProps('name')}
-      />
-      <TextInput
-        sx={{ maxWidth: 300 }}
-        label="Email"
-        type="email"
-        withAsterisk
-        icon={<IconAt size={14} />}
-        {...form.getInputProps('email')}
-      />
-      <PasswordInput
-        sx={{ maxWidth: 300 }}
-        label="Password"
-        withAsterisk
-        icon={<IconKey size={14} />}
-        {...form.getInputProps('password')}
-      />
-      <Button
-        type="submit"
-        variant="gradient"
-        gradient={{ from: 'teal', to: 'lime', deg: 105 }}
-      >
-        Register
-      </Button>
+    <>
+      <form onSubmit={form.onSubmit(handleSubmit)} autoComplete="off">
+        <TextInput
+          sx={{ maxWidth: 300 }}
+          label="Username"
+          type="text"
+          withAsterisk
+          icon={<IconUser size={14} />}
+          {...form.getInputProps('name')}
+        />
+        <TextInput
+          sx={{ maxWidth: 300 }}
+          label="Email"
+          type="email"
+          withAsterisk
+          icon={<IconAt size={14} />}
+          {...form.getInputProps('email')}
+        />
+        <PasswordInput
+          sx={{ maxWidth: 300 }}
+          label="Password"
+          withAsterisk
+          icon={<IconKey size={14} />}
+          {...form.getInputProps('password')}
+        />
+        <Button
+          type="submit"
+          variant="gradient"
+          gradient={{ from: 'teal', to: 'lime', deg: 105 }}
+        >
+          Register
+        </Button>
+      </form>
       {error && (
         <Alert icon={<IconAlertCircle size={16} />} title="Bummer!" color="red">
-          Something terrible happened! Invalid data entered.
+          {error}
         </Alert>
       )}
-    </form>
+    </>
   );
 }
